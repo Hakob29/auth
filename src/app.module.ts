5f800d6457c3d75abb14ca77bc202b3e12f8c36d
Hakob29 <hakobyan29001@gmail.com>
2022-12-02 21:48:24 +0400

    Orm Connection done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrmConfig } from 'ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true})],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot(OrmConfig.options)
+  ],
   controllers: [],
   providers: [],
 })
