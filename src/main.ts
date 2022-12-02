1d9d4b3aca33d12ad12cd835ba5a5b8f6995c247
Hakob29 <hakobyan29001@gmail.com>
2022-12-02 16:58:31 +0400

    Hasura Connection done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const host = config.get("HOST");
+  const port = config.get("PORT");
+  await app.listen(port || 3001, () => {
+    console.log(`Server has been connected to ${host+port}`);
+  });
 }
 bootstrap();
